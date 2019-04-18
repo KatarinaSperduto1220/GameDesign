b623ecee2d8359b994111a5b82899804c4dfc670
Katarina Sperduto <katarinasperduto@bellsouth.net>
2019-04-18 13:32:15 -0400

    WorkingModel

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+
+    BuildManager buildManager;
+
+    void Start()
+    {
+        Debug.Log("BM Instance Started");
+        buildManager = BuildManager.instance;
+    }
+    public void PurchaseStandardTurret()
+    {
+        Debug.Log("Standard Turret");
+        buildManager.SetTurretToBuild(buildManager.standardTurretPrefab);
+    }
+
+    public void PurchaseMissileLauncher()
+    {
+        Debug.Log("Missle Launcher");
+        buildManager.SetTurretToBuild(buildManager.missileLauncherPrefab);
+    }
+
+}
